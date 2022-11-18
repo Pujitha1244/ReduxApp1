103f6eac5466753046564e01dea8ec0be03d7a9e
saipujitha.v <saipujitha.v@kagamierp.com>
2022-11-18 21:45:54 +0530

    Second commit

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,8 +1,18 @@
 // Reducer will always take initial state and then action
+import { ActionTypes } from "../constants/action-types"
 
 const initialState = {
-    products: []
+    products: [{
+        id: 1,
+        title: "Poojitha",
+        category: "Developer"
+    }]
 }
-export const productReducer = (state, action)=>{ //initial state and action
-
+export const productReducer = (state = initialState, {type,payload})=>{ //initial state and action
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state
+        default:
+            return state
+    }
 }
